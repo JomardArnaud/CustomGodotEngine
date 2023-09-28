7217d9dd8a0d85c247c99548c6e9ed57bf46d186
JomardArnaud <arnaud.jomard@outlook.fr>
2023-09-28 18:19:21 +0700

    Arena scene v3.1 done, not fix the bug but detection and error message

--- Source/Arena/Arena.gd
+++ Source/Arena/Arena.gd
@@ -1,17 +1,8 @@
 @tool
 extends Polygon2D
 
-### TODO =>
-#______________________#
-
-@export var posArena : Vector2
-@export var colorArena : Color
-@export var colorWall : Color
 ## make a BETTER slider and listen the signal to auto update the border's Arena
 @export_range(0.1, 5, 0.1) var thicknessBorder = 0.5
-
-# Ok alors faire un algo qui permet de prendre le polygon, et faire un prolongement en partant du centrre du polygon
-# vers chaque vertice pour scale le polygon d'origin et produire celui qui sera le border
 
 static var DictStateEdge = {
 	current = Vector2(),
@@ -67,6 +58,8 @@ func algo(nbVertexPoly):
 		if (vertexId == 0):
 			firstSweetSpot = stateVertex["sweetSpot"]
 		#on ajoute les points pour la bordure extérieure
+		if stateVertex["sweetSpot"] == null:
+			printerr("Bad Arena's polygon, sweetSpot generation failed")
 		pointsExtBorder.append(stateVertex["sweetSpot"])
 #		# on set les buffer
 		bufferDict["current"] = stateVertex["current"]
@@ -76,6 +69,8 @@ func algo(nbVertexPoly):
 	# just adding all the polygon's vertex to finish the first block
 	for vertexId in range(nbVertexPoly - 1, -1, -1):
 		pointsExtBorder.append(self.polygon[vertexId])
+	# check if the border's pollygon collide on himself
+	checkBorderVertexHimselfCollission(pointsExtBorder)
 	var blockBorder = Block2D.new()
 	blockBorder.polygon = pointsExtBorder
 	blockBorder.set_poly(blockBorder)
@@ -110,3 +105,21 @@ func setSweetSpot(currentState: Dictionary, bufferState: Dictionary) -> void:
 		Vector2(currentState["edgeProlonged"].z, currentState["edgeProlonged"].w),
 		Vector2(bufferState["edgeProlonged"].x, bufferState["edgeProlonged"].y),
 		Vector2(bufferState["edgeProlonged"].z, bufferState["edgeProlonged"].w))
+
+func checkBorderVertexHimselfCollission(extBorder: PackedVector2Array) -> void:
+	var maxIdPointBorder = extBorder.size() - 1
+	# si l'algo est trop lent comme ça à cause de la complexité de cet aglo penser à optimiser ici d'abord
+	# on parse tout les vertex
+	for vertexId in range (0, maxIdPointBorder):
+		# et on check si le segement formé par vertex[vertexId] et vertex[vertexId + 1] collide avec les segments formé par les autres vertex
+		var i = (vertexId + 1) * int(vertexId < maxIdPointBorder) 
+		var currentSegment = Vector4(extBorder[vertexId].x, extBorder[vertexId].y, extBorder[i].x, extBorder[i].y)
+		while i != maxIdPointBorder:
+			var n = (i + 1) * int(i < maxIdPointBorder)
+			var tmpSegment = Vector4(extBorder[i].x, extBorder[i].y, extBorder[n].x, extBorder[n].y)
+			var collisionBorder = Geometry2D.segment_intersects_segment(Vector2(currentSegment.x, currentSegment.y), Vector2(currentSegment.z, currentSegment.w),
+				Vector2(tmpSegment.x, tmpSegment.y), Vector2(tmpSegment.z, tmpSegment.w))
+			if collisionBorder != null:
+				printerr("The border's pollygon collide on himself")
+				return
+			i = (i + 1) * int(i < maxIdPointBorder)
