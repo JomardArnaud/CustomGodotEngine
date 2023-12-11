5bfac2c86c3b4c53cecd8c597dba34d700e89012
JomardArnaud <arnaud.jomard@outlook.fr>
2023-12-11 12:38:59 +0900

    remove singletons to put their  behaviours in the main

--- Scenes/SC_Playground.gd
+++ Scenes/SC_Playground.gd
@@ -2,6 +2,5 @@ extends Scene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(infoPrevScene.playerPos)
 	pass # Replace with function body.
 
--- Singleton/SpriteManager.gd
+++
@@ -1,2 +0,0 @@
-class_name SpriteManager
-extends Object
