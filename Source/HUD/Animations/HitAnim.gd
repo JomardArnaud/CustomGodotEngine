f0f20034987e04d4668e8299debf40df1d83b1ca
JomardArnaud <arnaud.jomard@outlook.fr>
2023-12-20 00:30:17 +0900

    HitAnim v0.2

---
+++ Source/HUD/Animations/HitAnim.gd
@@ -0,0 +1,10 @@
+extends Control
+
+@export var duration: float
+@export var speed: float
+@export var distanceFromHit: float
+@export var colorText: Color 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
